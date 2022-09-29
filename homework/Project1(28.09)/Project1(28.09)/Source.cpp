155a32b94a0b574588e2ccaafdafc0e82ffc640b
Anastasiia Kharytonova <nharytonova@gmail.com>
2022-09-29 17:36:44 +0300

    add "Project1(28.09)" project files

---
+++ homework/Project1(28.09)/Project1(28.09)/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float R;
+	
+	cout << "enter R" << endl;
+	cin >> R;
+	cout << "C=" << 2 * 3.14 * R << endl;
+	cout << "S=" << 3.14 * R * R << endl;
+	cout << "V=" << 4.0 / 3 * 3.14 * R * R * R << endl;
+
+}
